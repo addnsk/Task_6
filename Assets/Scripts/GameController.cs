898e4763083206a2357da79733f4392a942d7eb9
addnsk <68299837+addnsk@users.noreply.github.com>
2020-07-15 17:52:45 +0700

    init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private Spawn[] _spawn;
+
+    void Start()
+    {
+        _spawn = FindObjectsOfType<Spawn>();
+
+        StartCoroutine("SpawnEnemies");
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            for (int i = 0; i < _spawn.Length; i++)
+            {
+                _spawn[i].CreatEnemyOnMap();
+
+                yield return new WaitForSeconds(2);
+
+                _spawn[i].RemoveEnemyOnMap();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+
+    private Rigidbody2D _enemyRb2D;
+
+
+    void Awake()
+    {
+        _enemyRb2D = _enemy.GetComponent<Rigidbody2D>();
+    }
+
+    public void CreatEnemyOnMap()
+    {
+        _enemy.transform.position = transform.position;
+
+        _enemy.SetActive(true);
+
+        _enemyRb2D.simulated = true;
+    }
+
+    public void RemoveEnemyOnMap()
+    {
+        _enemy.SetActive(false);
+
+        _enemyRb2D.simulated = false;
+    }
+}
